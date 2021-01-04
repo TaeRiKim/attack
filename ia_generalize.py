d55f37f55e26d4c1662752ceeacc3b40254bbc89
TaeRiKim <70612131+TaeRiKim@users.noreply.github.com>
2021-01-04 14:39:23 +0900

    Add files via upload

---
+++ ia_generalize.py
@@ -0,0 +1,67 @@
+def inversionattack(net, data, device, classes, num):
+    tp = transforms.Compose([
+    transforms.Resize(32),
+    transforms.CenterCrop(32),
+    transforms.ToTensor()
+    ])
+
+    def label_to_onehot(target, num_classes):
+        target = torch.unsqueeze(target, 1)
+        onehot_target = torch.zeros(target.size(0), num_classes, device=target.device)
+        onehot_target.scatter_(1, target, 1)
+        return onehot_target
+
+    def cross_entropy_for_onehot(pred, target):
+        return torch.mean(torch.sum(- target * F.log_softmax(pred, dim=-1), 1))
+    
+    net = model.to(device)
+    criterion = cross_entropy_for_onehot
+
+    gt_data = tp(data[num][0]).to(device)
+    gt_data = gt_data.view(1, *gt_data.size())
+    gt_label = torch.Tensor([data[num][1]]).long().to(device)
+    gt_label = gt_label.view(1, )
+    gt_onehot_label = label_to_onehot(gt_label, classes)
+
+    out = net(gt_data)
+    y = criterion(out, gt_onehot_label)
+    dy_dx = torch.autograd.grad(y, net.parameters())
+
+    '''
+    crutial?
+    '''
+    original_dy_dx = list((_.detach().clone() for _ in dy_dx))
+
+    dummy_data = torch.randn(gt_data.size()).to(device).requires_grad_(True)
+    dummy_label = torch.randn(gt_onehot_label.size()).to(device).requires_grad_(True)
+
+    optimizer = torch.optim.LBFGS([dummy_data, dummy_label] )
+
+    finalout = []
+    for iters in range(300):
+        def closure():
+            optimizer.zero_grad()
+
+            pred = net(dummy_data) 
+            dummy_onehot_label = F.softmax(dummy_label, dim=-1)
+            dummy_loss = criterion(pred, dummy_onehot_label) # TODO: fix the gt_label to dummy_label in both code and slides.
+            dummy_dy_dx = torch.autograd.grad(dummy_loss, net.parameters(), create_graph=True)
+            
+            grad_diff = 0
+            grad_count = 0
+            for gx, gy in zip(dummy_dy_dx, original_dy_dx): # TODO: fix the variablas here
+                grad_diff += ((gx - gy) ** 2).sum()
+                grad_count += gx.nelement()
+            # grad_diff = grad_diff / grad_count * 1000
+            grad_diff.backward()
+                        
+            return grad_diff
+
+        optimizer.step(closure)
+        
+        finalout = dummy_data[0].cpu()
+    
+    return finalout
+        
+        
+
